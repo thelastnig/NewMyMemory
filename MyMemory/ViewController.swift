4341eb99ab49379090641b7789d7fd87cb263150
PJW <PJW>
2019-10-09 14:28:47 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by PJW on 09/10/2019.
+//  Copyright © 2019 PJW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
